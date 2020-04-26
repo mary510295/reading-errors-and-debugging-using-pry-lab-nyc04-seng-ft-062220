194383750004f66e5f6183163f0c579fe8af2611
Fu <fu.goto@gmail.com>
2020-04-26 20:30:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,13 +1,13 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-    binding.pry
+    
   if string[0] == "s"
     10 * "s" + string
   else
     string
   end
-
+binding.pry
 end
 
 snake_it_up("so")
