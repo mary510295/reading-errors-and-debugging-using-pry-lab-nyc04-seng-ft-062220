f2955589fc5c990538e04f8a9ac96e35c088a1d9
Fu <fu.goto@gmail.com>
2020-04-26 20:46:50 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
-
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    10.times do string = 's' + string
+    end
+    string
   else
     string
   end
