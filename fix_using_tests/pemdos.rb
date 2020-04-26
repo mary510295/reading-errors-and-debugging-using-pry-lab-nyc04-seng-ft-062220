d62405e70b39cf0213b640e101b823d032267e7d
Fu <fu.goto@gmail.com>
2020-04-26 20:35:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,9 +3,11 @@ require 'pry'
 def snake_it_up(string)
     
   if string[0] == "s"
-    string_start = 10.times do
-    10 * "s" + string
-
+    string_start = 
+    10.times do 's'
+  end
+    string = string_start + string
+binding.pry
   else
     string
   end
